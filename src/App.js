204026b2205806ae2974fbbcc021ffae5b14f6f4
MD AWAIZ <awaizkhanmd@gmail.com>
2022-12-08 23:23:28 +0530

    React-01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import logo from './logo.svg';
+import { useState } from 'react'
+import "./App.css";
+import Alert from './components/Alert';
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+//import About from "./components/About";
 function App() {
+  const [mode, setMode] = useState('light');
+  const [Red, setRed] = useState('light');
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+
+     setAlert({
+      msg:message,
+      type:type
+     })
+     setTimeout(() => {
+      setAlert(null);
+  }, 1500);
+  }
+
+  const toggleMode = function() {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#042743'
+      showAlert("dark mode enabled","success")
+  }
+else{
+    setMode('light')
+    document.body.style.backgroundColor='white'
+    showAlert("light mode enabled","success")
+  }
+  }
+  const clicked=function(){
+    if(mode === 'light'){
+      setMode('dark')
+    }
+  }
+
+  const Switch = function(){
+    if(mode === 'light'){
+      setMode('#dc3545')
+      document.body.style.backgroundColor='#042743'
+      showAlert("dark mode enabled","success")
+  }
+  else
+  {
+    setMode('light')
+    document.body.style.backgroundColor='white'
+    showAlert("light mode enabled","success")
+  }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtil" mode={mode} toggleMode={toggleMode} Switch={Switch}  clicked={clicked}/>
+      <Alert  alert={alert}/>
+      <div className="container  my-3">
+        <TextForm showAlert={showAlert}  heading="enter the text"  mode={mode}/>
+        {/* <About /> */}
+      </div>
+    </>
   );
 }
 
